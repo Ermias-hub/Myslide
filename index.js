aa05bf6a877e4d4861837a5af7a6f2c43bdbbd58
Ermias Aron <aroneru3@gmail.com>
2019-12-05 12:54:53 +0200

    first heroku app

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + "/slide.html");
+});
+
+app.get('/img1.jpg',(req,res)=>{
+    res.sendFile(__dirname + "/img1.jpg");
+});
+
+app.get('/img2.jpg',(req,res)=>{
+    res.sendFile(__dirname + "/img2.jpg");
+});
+
+app.get('/img3.jfif',(req,res)=>{
+    res.sendFile(__dirname + "/img3.jfif");
+})
+
+
+
+
+app.listen(3000,() => console.log('listening on port 3000'));
